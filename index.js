011a292c673d4cf114566ed8daa64cba83009bca
TNSahid <sahid.miller@gmail.com>
2021-07-09 11:14:42 -0400

    fix: root package for local testing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  https: require("@network-stackify/https"),
-  http: require("@network-stackify/http"),
-  net: require("@network-stackify/libp2p-net"),
-  tls: require("@network-stackify/tls"),
+  https: require("./packages/https"),
+  http: require("./packages/http"),
+  net: require("./packages/libp2p-net"),
+  tls: require("./packages/tls"),
 };
